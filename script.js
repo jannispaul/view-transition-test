6584c5565818fcb3e4370c4bec57329af35b6a48
jannispaul <jannis@pixel-pink.de>
2024-03-03 16:59:05 +0100

    removed script

--- script.js
+++
@@ -1,3 +0,0 @@
-let slug = location.pathname; //.split("/").slice(1);
-let currentLinks = document.querySelectorAll('a[href="' + slug + '"]');
-currentLinks.forEach((link) => (link.className += " current"));
